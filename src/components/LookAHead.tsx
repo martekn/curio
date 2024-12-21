b6cb9dcb955a3cc80566c88a3dbb79521efbd895
martekn <marte.mk@hotmail.com>
2024-12-21 23:38:44 +0100

    Add search and look-a-head component features

---
+++ src/components/LookAHead.tsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import { Product } from "@/types/types";
+
+const LookAHead = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    border: 1px solid red;
+    z-index: 100;
+    position: absolute;
+    border-radius: ${theme.tokens.borderRadius};
+    background-color: ${theme.tokens.colorBackgroundDefault};
+  `}
+`;
+const LookAHeadText = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.sizes.size3} ${theme.sizes.size7};
+    font-size: ${theme.typography.fontSize200};
+    font-weight: ${theme.typography.fontWeightMedium};
+    color: ${theme.colors.light.neutral400};
+  `}
+`;
+const LookAHeadList = styled.ul``;
+const LookAHeadListItem = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: ${theme.sizes.size4};
+    z-index: 2;
+    padding: ${theme.sizes.size3} ${theme.sizes.size7};
+  `}
+`;
+const LookAHeadListItemImage = styled.img`
+  ${({ theme }) => css`
+    border-radius: ${theme.tokens.borderRadius};
+    pointer-events: none;
+    height: 3rem;
+    width: 3rem;
+    object-fit: cover;
+  `}
+`;
+const LookAHeadListItemTitle = styled.a`
+  ${({ theme }) => css`
+    color: ${theme.tokens.colorTextDefault};
+    font-family: ${theme.tokens.colorTextDefault};
+    font-size: ${theme.tokens.bodyFontSize};
+    text-decoration: underline transparent;
+    text-underline-offset: 2px;
+    transition: text-decoration 200ms ease-in-out;
+
+    &:hover {
+      text-decoration-color: ${theme.tokens.colorTextDefault};
+    }
+
+    &::before {
+      transition: background-color 200ms ease-in-out;
+      display: block;
+      content: "";
+      position: absolute;
+      inset: 0;
+      cursor: pointer;
+      z-index: -1;
+    }
+
+    &:hover::before {
+      background-color: ${theme.colors.light.primary100};
+    }
+  `}
+`;
+
+/**
+ * A component that displays the look-ahead search results for products based on the search term.
+ * It limits the displayed results to the top 4, showing the title "Showing top 4 results",
+ * or a message indicating no results are found or displaying all available results.
+ * Intended to be used with the SearchBar component.
+ *
+ * @param props - The props for the component.
+ * @param props.productsSearchResult - An array of products that match the search query.
+ *
+ * @returns The rendered `LookAHeadContainer` with the search results.
+ *
+ * @example
+ * <LookAHeadContainer productsSearchResult={matchingProducts} />
+ */
+export const LookAHeadContainer = ({ productsSearchResult }: { productsSearchResult: Product[] }) => {
+  const [title, setTitle] = useState("Showing top 4 results");
+  const [topMatches, setTopMatches] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (!productsSearchResult || productsSearchResult.length === 0) {
+      setTopMatches([]);
+      setTitle("No products found");
+    } else if (productsSearchResult.length > 4) {
+      setTopMatches(productsSearchResult.splice(0, 4));
+      setTitle("Showing top 4 results");
+    } else {
+      setTopMatches(productsSearchResult);
+      setTitle("Showing all results");
+    }
+  }, [productsSearchResult]);
+
+  return (
+    <LookAHead>
+      <LookAHeadText>{title}</LookAHeadText>
+      <LookAHeadList>
+        {topMatches.map((product) => (
+          <LookAHeadListItem key={product.id}>
+            <LookAHeadListItemImage src={product.image.url} alt={product.image.alt} />
+            <LookAHeadListItemTitle href={`/product/${product.id}`}>{product.title}</LookAHeadListItemTitle>
+          </LookAHeadListItem>
+        ))}
+      </LookAHeadList>
+    </LookAHead>
+  );
+};
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,121 @@
+import styled, { css } from "styled-components";
+import { Search } from "react-feather";
+import { ChangeEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { debounce } from "@/utils/debounce";
+import { useFetch } from "@/hooks/useFetch";
+import { LookAHeadContainer } from "./LookAHead";
+import { Product, ProductsFetch } from "@/types/types";
+
+const SearchContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 100%;
+    color: ${theme.tokens.formInputColor};
+    font-family: ${theme.tokens.formInputFontFamily};
+    font-weight: ${theme.tokens.formInputFontWeight};
+    font-size: ${theme.tokens.formInputFontSize};
+    background-color: ${theme.tokens.formInputBackgroundColor};
+    border: ${theme.tokens.formInputBorderDefault};
+    border-color: ${theme.colors.light.neutral300};
+    padding: ${theme.tokens.formInputPadding};
+    border-radius: ${theme.tokens.formInputBorderRadius};
+
+    &:focus-within {
+      outline: ${theme.tokens.formInputOutlineFocus};
+      border: ${theme.tokens.formInputBorderFocus};
+    }
+  `}
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 1;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Icon = styled.button`
+  background-color: transparent;
+  > * {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+`;
+
+const Form = styled.form`
+  position: relative;
+`;
+
+/**
+ * A search bar component that allows users to search products.
+ * It includes a debounced look-ahead feature that displays suggestions as the user types.
+ * The component also handles form submission and redirects to the search results page.
+ *
+ * @returns The rendered `SearchBar` component.
+ *
+ * @example
+ * <SearchBar />
+ */
+export const SearchBar = () => {
+  const { data } = useFetch<ProductsFetch>("https://v2.api.noroff.dev/online-shop", "get");
+  const [productsSearchResult, setProductsSearchResult] = useState<Product[]>([]);
+  const [showLookAHead, setShowLookAHead] = useState(false);
+
+  const { register, handleSubmit } = useForm<{ searchInput: string }>();
+  const router = useRouter();
+
+  /**
+   * Updates the look-ahead search results based on the user's input.
+   * Debounces the execution to limit how frequently it processes user input.
+   *
+   * @param {ChangeEvent<HTMLInputElement>} formData - The input change event containing the search term.
+   */
+  const updateLookAHead = debounce((formData: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = formData.target.value.toLowerCase();
+
+    if (!searchTerm) {
+      setProductsSearchResult([]);
+      setShowLookAHead(false);
+      return;
+    }
+
+    if (data) {
+      const products = data.data;
+      const matchingProducts = products.filter((product) => product.title.toLowerCase().includes(searchTerm));
+      setShowLookAHead(true);
+      setProductsSearchResult(matchingProducts);
+    }
+  }, 500);
+
+  /**
+   * Handles the form submission for the search bar.
+   * This function is triggered when the user submits the form.
+   *
+   * @param formData - The data object containing form values.
+   * @param formData.searchInput - The search query input from the user.
+   */
+  const onSubmit = (formData: { searchInput: string }) => {
+    if (formData.searchInput) {
+      const query = formData.searchInput;
+      router.push(`/products/search/${query}`);
+    } else {
+      router.push(`/products`);
+    }
+  };
+
+  return (
+    <Form>
+      <SearchContainer onSubmit={handleSubmit(onSubmit)}>
+        <Input {...register("searchInput")} onChange={(formData) => updateLookAHead(formData)} />
+        <Icon>
+          <Search />
+        </Icon>
+      </SearchContainer>
+      {showLookAHead && <LookAHeadContainer productsSearchResult={productsSearchResult} />}
+    </Form>
+  );
+};

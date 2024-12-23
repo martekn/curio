bcf7162b17882c882f8563ed140b378f46438014
martekn <marte.mk@hotmail.com>
2024-12-23 22:02:28 +0100

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Key used for storing and retrieving cart data in local storage.
+ * @constant
+ */
+export const CART_STORAGE_KEY = "cart";
+
+/**
+ * Base URL for API requests to the Noroff API.
+ * @constant
+ */
+export const API_URL = "https://v2.api.noroff.dev/";

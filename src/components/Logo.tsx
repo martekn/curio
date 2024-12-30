79f775c81b268344e83b47ddd7ea84242db6c446
martekn <marte.mk@hotmail.com>
2024-12-30 18:17:32 +0100

    Create logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -20,9 +20,29 @@ const Logo_lg = styled(Logo_base)`
   `}
 `;
 
-export const Logo = ({ size = "lg" }: { size: "sm" | "lg" }) => {
+interface Props {
+  size: "sm" | "lg";
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+}
+
+/**
+ * Renders a logo in either small or large size with an optional click handler.
+ *
+ * @param [size="lg"] - The size of the logo. Use "sm" for small or "lg" for large (default).
+ * @param [onClick] - A callback function triggered when the logo is clicked. Receives a React mouse event as its argument.
+ * @returns  The rendered Logo component.
+ */
+export const Logo = ({ size = "lg", onClick }: Props) => {
   if (size === "sm") {
-    return <Logo_sm href="/">Curio</Logo_sm>;
+    return (
+      <Logo_sm href="/" onClick={onClick}>
+        Curio
+      </Logo_sm>
+    );
   }
-  return <Logo_lg href="/">Curio</Logo_lg>;
+  return (
+    <Logo_lg href="/" onClick={onClick}>
+      Curio
+    </Logo_lg>
+  );
 };
